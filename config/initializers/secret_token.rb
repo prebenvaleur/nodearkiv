3631cecf7958326f2be51248ec64e8b76ff48f24
Preben Valeur <preben_valeur@mail.tele.dk>
2010-12-26 21:27:25 +0100

    added stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nodearkiv::Application.config.secret_token = 'e642dd05c52d421c585ee025c5802eccbd536b060f5bb32bf9d4f719ff8e9faee7d1625700f04c96168ddf1d5109d1f2ec456701d6c38fcf3a66f9246e54d71d'
